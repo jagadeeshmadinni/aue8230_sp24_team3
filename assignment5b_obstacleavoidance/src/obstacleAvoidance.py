109e30147f7e5d2c3df119303b0d3d7ca4a5bb31
saumil2396 <100736973+saumil2396@users.noreply.github.com>
2024-03-22 16:25:07 -0400

    Update obstacleAvoidance.py
    Signed-off-by: saumil2396 <100736973+saumil2396@users.noreply.github.com>

--- assignment5b_obstacleavoidance/src/obstacleAvoidance.py
+++ assignment5b_obstacleavoidance/src/obstacleAvoidance.py
@@ -6,38 +6,6 @@ from math import pow, atan2, sqrt, pi
 import numpy as np
 import time
 
-'''
-def input_finder_linear(optimum):
-    match optimum:
-        case 0:
-            return 0.2
-        case 1:
-            return 0.2
-        case 2:
-            return 0
-        case 3:
-            return 0
-        case 4:
-            return 0.2
-        case _:
-            return 0
-
-def input_finder_lateral(optimum):
-    match optimum:
-        case 0:
-            return 0
-        case 1:
-            return 1
-        case 2:
-            return 1
-        case 3:
-            return 1
-        case 4:
-            return 1
-        case _:
-            return 0
-'''
-
 def callback(msg):
     dists = np.array([msg.ranges[0],msg.ranges[30],msg.ranges[90],msg.ranges[270],msg.ranges[330]])    
     dist = np.where(dists > 3.5, 3.5, dists)
