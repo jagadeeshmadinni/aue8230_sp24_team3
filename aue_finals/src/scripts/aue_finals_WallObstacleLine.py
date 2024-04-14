9ef4187161d4f5c7200fdbcab297519f1aaee78a
saumil2396 <100736973+saumil2396@users.noreply.github.com>
2024-04-14 13:58:51 -0400

    Added and updated files for aue_finals.py (#26)
    * Updated wall_obstacle
    
    * Added updated files for aue_finals.py

--- aue_finals/src/scripts/aue_finals_WallObstacleLine.py
+++ aue_finals/src/scripts/aue_finals_WallObstacleLine.py
@@ -2,7 +2,9 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
-from wall_obstacle import WallFollower
+import numpy as np
+from wall_obstacle_line import WallFollower
+from line import LineFollower
 
 class Autonomy_Final:
 
@@ -11,16 +13,13 @@ class Autonomy_Final:
         self.velocity = Twist()
         zeros = [0]*360
         self.lidar_data = zeros
+        self.cv_image = np.zeros((480,640,3), dtype=np.uint8)
         self.Velocity_Publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.Range_Subscriber = rospy.Subscriber("/scan", LaserScan, self.range_callback)
         #self.Image_Subscriber = rospy.Subscriber("/camera/rgb/image_raw",Image,self.Line_Following_object.camera_callback)
         rospy.on_shutdown(self.Shutdown_callback)
         self.last_cmdvel_command = Twist()
-<<<<<<< HEAD
         #self._cmdvel_pub_rate = rospy.Rate(1)
-=======
-        #self._cmdvel_pub_rate = rospy.Rate(3)
->>>>>>> 2bb1907b1f07648ae308df6c2e73cb4bacf489d9
 
     def Shutdown_callback(self):
         print("Shutting down now")
@@ -37,7 +36,6 @@ class Autonomy_Final:
          
     def move_robot(self, twist_object):
         self.Velocity_Publisher.publish(twist_object)
-<<<<<<< HEAD
         #self._cmdvel_pub_rate.sleep()
             #current_equal_to_new = self.compare_twist_commands(twist1=self.last_cmdvel_command,twist2=twist_object)
     
@@ -48,22 +46,47 @@ class Autonomy_Final:
         self.move_robot(twist_object)'''
 
 
-=======
-                                    
->>>>>>> 2bb1907b1f07648ae308df6c2e73cb4bacf489d9
     def motion_planner(self):
         # Start the supervisory node
         #self.Autonomy = Autonomy_Final()
         #lidar_scan = list(data_lidar.ranges[0:359])
         endMission = False
         switch_flag = True
+        previous = 0
+        stop_sign = False
+        stop_sign_threshold = 0.5
         while not rospy.is_shutdown():
             print("Mission commenced")
             while(not endMission):
+                # Create CV Image here:
+                # ENTER CODE
+                #self.cv_image[:,0:50] = (255,0,0)
+                #self.cv_image[:,51:99] = (0,255,0)
+
                 # Check for obstacles and line
                 Wall_Following_object = WallFollower()
-                cmd_vel = Wall_Following_object.follow_wall(data=self.lidar_data)
-                self.move_robot(cmd_vel)
+                Line_Following_object = LineFollower()
+                cmd_vel_wall, front = Wall_Following_object.follow_wall(data=self.lidar_data)
+                cmd_vel_line, error_line = Line_Following_object.track_line(img=self.cv_image)
+
+                # Velocity commands
+                self.velocity.angular.z = cmd_vel_wall.angular.z + cmd_vel_line.angular.z
+                self.velocity.linear.x = cmd_vel_wall.linear.x
+
+                # Check for the error published by line following algorithm
+                error_diff =  np.append(previous,error_line)
+                if np.diff(error_diff) != 0:
+                    self.velocity.linear.x = cmd_vel_line.linear.x
+                
+                # Check for stop sign
+                if stop_sign == True and front < stop_sign_threshold:
+                    self.velocity.linear = 0
+                    self.velocity.angular.z = 0
+                    rate = rospy.Rate(3)
+                    rate.sleep()
+
+                self.move_robot(self.velocity)
+                previous = error_line
 
 if __name__ == "__main__":
     jarvis = Autonomy_Final()
