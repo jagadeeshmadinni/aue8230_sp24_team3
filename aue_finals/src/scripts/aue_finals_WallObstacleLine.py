bc8e85bb55c5e7440ebf262ca9b3ed53e915c1e2
JohirSuresh22 <158509706+JohirSuresh22@users.noreply.github.com>
2024-04-15 23:35:39 -0400

    T-2 day Updates (#27)
    * some minor change in autonomy launch
    
    * T-2 day updates

--- aue_finals/src/scripts/aue_finals_WallObstacleLine.py
+++ aue_finals/src/scripts/aue_finals_WallObstacleLine.py
@@ -2,10 +2,14 @@
 import rospy
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Int32MultiArray
+from std_msgs.msg import Bool
 import numpy as np
 from wall_obstacle_line import WallFollower
 from line import LineFollower
-
+import imagezmq
+import cv2
+from stop_sign import StopSign
 class Autonomy_Final:
 
     def __init__(self):
@@ -13,9 +17,10 @@ class Autonomy_Final:
         self.velocity = Twist()
         zeros = [0]*360
         self.lidar_data = zeros
-        self.cv_image = np.zeros((480,640,3), dtype=np.uint8)
         self.Velocity_Publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
         self.Range_Subscriber = rospy.Subscriber("/scan", LaserScan, self.range_callback)
+        self.stop_sign_pub = rospy.Publisher('/stop_sign', Bool, queue_size=10)
+        self.bbox_pub = rospy.Publisher('/bbox',Int32MultiArray,queue_size=10)
         #self.Image_Subscriber = rospy.Subscriber("/camera/rgb/image_raw",Image,self.Line_Following_object.camera_callback)
         rospy.on_shutdown(self.Shutdown_callback)
         self.last_cmdvel_command = Twist()
@@ -53,40 +58,46 @@ class Autonomy_Final:
         endMission = False
         switch_flag = True
         previous = 0
-        stop_sign = False
+        stop_sign_flag = False
         stop_sign_threshold = 0.5
+        image_hub = imagezmq.ImageHub()
+        Wall_Following_object = WallFollower()
+        Line_Following_object = LineFollower()
+        Stop_Sign_Object = StopSign()
+        print("Mission commenced")
+        rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            print("Mission commenced")
-            while(not endMission):
-                # Create CV Image here:
-                # ENTER CODE
-                #self.cv_image[:,0:50] = (255,0,0)
-                #self.cv_image[:,51:99] = (0,255,0)
 
-                # Check for obstacles and line
-                Wall_Following_object = WallFollower()
-                Line_Following_object = LineFollower()
-                cmd_vel_wall, front = Wall_Following_object.follow_wall(data=self.lidar_data)
-                cmd_vel_line, error_line = Line_Following_object.track_line(img=self.cv_image)
+            # Check for obstacles and line
 
-                # Velocity commands
-                self.velocity.angular.z = cmd_vel_wall.angular.z + cmd_vel_line.angular.z
-                self.velocity.linear.x = cmd_vel_wall.linear.x
+            rpi_name, cv_image = image_hub.recv_image()
+            cv2.waitKey(1)
+            image_hub.send_reply(b'OK')
+            #cv2.imshow(rpi_name, cv_image)
+            cmd_vel_wall, front = Wall_Following_object.follow_wall(data=self.lidar_data)   
+            cmd_vel_line, error_line = Line_Following_object.track_line(flipped_img=cv_image)
+            stop_sign_flag,bbox_coords = Stop_Sign_Object.stop_sign_detect(cv_image)
+            self.stop_sign_pub.publish(stop_sign_flag)
+            self.bbox_pub.publish(Int32MultiArray(data=bbox_coords))
 
-                # Check for the error published by line following algorithm
-                error_diff =  np.append(previous,error_line)
-                if np.diff(error_diff) != 0:
-                    self.velocity.linear.x = cmd_vel_line.linear.x
-                
-                # Check for stop sign
-                if stop_sign == True and front < stop_sign_threshold:
-                    self.velocity.linear = 0
-                    self.velocity.angular.z = 0
-                    rate = rospy.Rate(3)
-                    rate.sleep()
+            # Velocity commands
+            #self.velocity.angular.z = cmd_vel_wall.angular.z
+            #self.velocity.linear.x = cmd_vel_wall.linear.x
 
-                self.move_robot(self.velocity)
-                previous = error_line
+            # Check for the error published by line following algorithm
+            #error_diff =  np.append(previous,error_line)
+            #if np.diff(error_diff) != 0:
+            self.velocity.linear.x = cmd_vel_line.linear.x
+            self.velocity.angular.z = cmd_vel_line.angular.z
+            # Check for stop sign
+            '''if stop_sign_flag == True and front < stop_sign_threshold:
+                self.velocity.linear = 0
+                self.velocity.angular.z = 0
+                rospy.sleep(3)'''
+
+            self.move_robot(self.velocity)
+            #previous = error_line
+            #rate.sleep()
 
 if __name__ == "__main__":
     jarvis = Autonomy_Final()
